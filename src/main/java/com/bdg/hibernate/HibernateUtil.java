130c056cc4e6195a0432734b88ece0a68a0238d6
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-04-20 20:59:07 +0400

    Optimized HibernateUtil.java class.

--- src/main/java/com/bdg/hibernate/HibernateUtil.java
+++ src/main/java/com/bdg/hibernate/HibernateUtil.java
@@ -1,20 +1,32 @@
 package com.bdg.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory = null;
 
-    static {
-        Configuration cfg = new Configuration().configure();
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                .applySettings(cfg.getProperties());
-        sessionFactory = cfg.buildSessionFactory(builder.build());
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            return configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
     }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        sessionFactory.close();
     }
 }
