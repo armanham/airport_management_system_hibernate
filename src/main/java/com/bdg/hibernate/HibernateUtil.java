44e74efab16165d0a7811306cddbc4be557c99d5
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-04-28 00:21:16 +0400

    Updated HibernateUtil.java class, changed exception type in catch block of buildSessionFactory() method.

--- src/main/java/com/bdg/hibernate/HibernateUtil.java
+++ src/main/java/com/bdg/hibernate/HibernateUtil.java
@@ -10,13 +10,11 @@ public class HibernateUtil {
 
     private static SessionFactory buildSessionFactory() {
         try {
-            // Create the SessionFactory from hibernate.cfg.xml
             Configuration configuration = new Configuration();
             configuration.configure("META-INF/hibernate.cfg.xml");
             return configuration.buildSessionFactory();
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
+        } catch (Exception ex) {
+            System.out.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
