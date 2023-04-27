528257d5c02ffcd6e75d23a8c6bc70b7bda294d3
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-04-28 00:17:50 +0400

    Added validateString() method in Validator.java class.

--- src/main/java/com/bdg/validator/Validator.java
+++ src/main/java/com/bdg/validator/Validator.java
@@ -2,6 +2,7 @@ package com.bdg.validator;
 
 public class Validator {
 
+
     public static void checkId(int id) {
         if (id <= 0) {
             throw new IllegalArgumentException("'id' must be a positive number: ");
@@ -12,6 +13,14 @@ public class Validator {
     public static void checkNull(Object item) {
         if (item == null) {
             throw new NullPointerException("Passed null value as 'item': ");
+        }
+    }
+
+
+    public static void validateString(String str) {
+        checkNull(str);
+        if (str.isEmpty()) {
+            throw new IllegalArgumentException("Passed empty value as 'str': ");
         }
     }
 }
