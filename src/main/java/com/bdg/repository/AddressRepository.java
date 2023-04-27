f31603e844e20a9ff8b66b73fd93a248a15654d1
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-04-28 00:35:18 +0400

    Added specific abstract methods in AddressRepository.java interface.

--- src/main/java/com/bdg/repository/AddressRepository.java
+++ src/main/java/com/bdg/repository/AddressRepository.java
@@ -5,4 +5,7 @@ import com.bdg.repository.common.CommonRepository;
 
 public interface AddressRepository extends CommonRepository<AddressMod> {
 
+    int getId(AddressMod addressMod);
+
+    boolean exists(AddressMod addressMod);
 }
