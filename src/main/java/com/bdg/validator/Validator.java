3785aa5b751a629075636bdddeffc139d533b93c
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-04-30 02:51:51 +0400

    Deleted some empty lines.

--- src/main/java/com/bdg/validator/Validator.java
+++ src/main/java/com/bdg/validator/Validator.java
@@ -2,20 +2,17 @@ package com.bdg.validator;
 
 public class Validator {
 
-
     public static void checkId(int id) {
         if (id <= 0) {
             throw new IllegalArgumentException("'id' must be a positive number: ");
         }
     }
 
-
     public static void checkNull(Object item) {
         if (item == null) {
             throw new NullPointerException("Passed null value as 'item': ");
         }
     }
-
 
     public static void validateString(String str) {
         checkNull(str);
